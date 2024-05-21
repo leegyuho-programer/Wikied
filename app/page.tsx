d288e31fbc6c3a192aa9335440a82eaba54f7b54
Gyuho Lee <j980303@naver.com>
2024-05-22 00:38:26 +0900

    feat: Input 공용 컴포넌트 완성

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Input from './../components/Input/Input';
+import styles from './page.module.css';
 
 export default function Home() {
-  return <Input />
+  return <div className={styles.container}>hi</div>;
 }
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,5 +1,22 @@
-function Input() {
-  return <input />
+import styles from './Input.module.css';
+import { useForm } from 'react-hook-form';
+
+interface InputProps {
+  name: string;
+  label?: string;
+  type?: string;
+}
+
+function Input({ name, label, type = 'text' }): InputProps {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input className={styles.input} id={name} type={type} />
+      {/* {errors && <span className={styles.error}>error</span>} */}
+    </div>
+  );
 }
 
 export default Input;
