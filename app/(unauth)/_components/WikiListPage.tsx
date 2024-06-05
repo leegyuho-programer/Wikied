4c17af0b858d60eecbf39b5e8b6b1657c228617f
Gyuho Lee <j980303@naver.com>
2024-06-06 01:44:52 +0900

    feat: WikiListPage UI 완성

--- app/(unauth)/_components/WikiListPage.tsx
+++ app/(unauth)/_components/WikiListPage.tsx
@@ -1,28 +1,31 @@
 import Link from '../../../components/Link/Link';
 import SearchBar from '../../../components/SearchBar/SearchBar';
-import ProfileIcon from '../../../components/SvgComponents/ProfileIcon/ProfileIcon';
-import Pagination from './Pagination/Pagenation';
+import WikiIcon from '../../../components/SvgComponents/WikiIcon/WikiIcon';
+import Pagination from './Pagination/Pagination';
 import styles from './WikiListPage.module.css';
 
 function WikiListPage() {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.search}>
         <SearchBar />
-        <p>"동욱"님을 총 3명 찾았습니다.</p>
+        <p className={styles.text}>"동욱"님을 총 3명 찾았습니다.</p>
       </div>
-      <div>
-        <div>
+      <div className={styles.wikiBox}>
+        <div className={styles.profile}>
           <div>
-            <ProfileIcon />
+            <WikiIcon />
           </div>
-          <div>
-            <p>김동욱</p>
-            <p>서울, 대한민국</p>
-            <p>대학생</p>
+          <div className={styles.intro}>
+            <p className={styles.name}>김동욱</p>
+            <p className={styles.data}>
+              서울, 대한민국
+              <br />
+              대학생
+            </p>
           </div>
         </div>
-        <div>
+        <div className={styles.link}>
           <Link />
         </div>
       </div>
