6f721a579618252a3a5dd7c9fc88ea76be87c333
Gyuho Lee <j980303@naver.com>
2024-06-05 23:15:15 +0900

    fix: 버튼 컴포넌트 반응형 수정

---
+++ app/(unauth)/_components/UserPage.tsx
@@ -0,0 +1,35 @@
+import Button from '../../../components/Button/Button';
+import Link from '../../../components/Link/Link';
+import SideBar from '../../../components/SideBar/SideBar';
+import styles from './UserPage.module.css';
+
+function UserPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        {/* 이름 데이터 가져오기 */}
+        <p className={styles.name}>이지동</p>
+        <Link />
+      </div>
+      <div className={styles.section}>
+        {/* 데이터가 있는지 없는지에 따라 다르게 보이게 하기 */}
+        <SideBar />
+        <div className={styles.nodata}>
+          <p className={styles.text}>
+            아직 작성된 내용이 없네요.
+            <br />
+            친구들을 위키로 초대해 보세요!
+          </p>
+          <Button isLink={true} destination="/signup" variant="primary" size="XS">
+            시작하기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPage;
+
+// 내 위키 페이지
+// 유저 위키 페이지
