4b81ded53307f891f23524e7acec38b3332b4877
Gyuho Lee <j980303@naver.com>
2024-06-07 22:01:09 +0900

    feat: 환경변수 추가

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
