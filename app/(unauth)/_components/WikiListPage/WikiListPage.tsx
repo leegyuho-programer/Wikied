0159ae9ab246c855b966f69383fba57f092b6f4d
Gyuho Lee <j980303@naver.com>
2024-06-07 22:05:41 +0900

    fix: 큰 따옴표 에러 수정

--- app/(unauth)/_components/WikiListPage/WikiListPage.tsx
+++ app/(unauth)/_components/WikiListPage/WikiListPage.tsx
@@ -14,7 +14,7 @@ function WikiListPage() {
     <div className={styles.container}>
       <div className={styles.search}>
         <SearchBar />
-        <p className={styles.text}>"동욱"님을 총 3명 찾았습니다.</p>
+        <p className={styles.text}>&8quot 동욱 &ldquo님을 총 3명 찾았습니다.</p>
       </div>
       <div className={styles.wikiBox}>
         <div className={styles.profile}>
