5c38243bedac33bd43aea371da5d20afccf4faf1
Gyuho Lee <j980303@naver.com>
2024-06-11 19:02:35 +0900

    feat: profile 타입 추가

--- types/profile.ts
+++ types/profile.ts
@@ -1,4 +1,40 @@
-interface PostProfile {
+interface User {
+  updatedAt: string;
+  job: string;
+  nationality: string;
+  city: string;
+  image: string;
+  code: string;
+  name: string;
+  id: number;
+}
+
+interface PostProfileRequestType {
   securityAnswer: string;
   securityQuestion: string;
+}
+
+interface PostProfileResponseType {
+  updatedAt: string;
+  securityQuestion: string;
+  teamId: string;
+  content: string;
+  nationality: string;
+  family: string;
+  bloodType: string;
+  nickname: string;
+  birthday: string;
+  sns: string;
+  job: string;
+  mbti: string;
+  city: string;
+  image: string;
+  code: string;
+  name: string;
+  id: number;
+}
+
+interface GetProfileResponseType {
+  totalCount: number;
+  list: User[];
 }
