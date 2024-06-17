89d31df3465ef20a0f887cd2a40627c1b9d7761a
Gyuho Lee <j980303@naver.com>
2024-06-17 11:09:58 +0900

    fix: menu 컴포넌트 로그인 상태일 때와 로그아웃 상태일 때 다르게 보이도록 수정

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,7 +1,36 @@
+// 'use client';
+
+// import Link from 'next/link';
+// import styles from './Menu.module.css';
+
+// interface Props {
+//   onMenuClick: () => void;
+//   onLogout: () => void;
+// }
+
+// function Menu({ onMenuClick, onLogout }: Props) {
+//   return (
+//     <div className={styles.container}>
+//       <Link href="/myAccount" className={styles.menuItem} onClick={onMenuClick}>
+//         계정 설정
+//       </Link>
+//       <Link href="/mypage" className={styles.menuItem} onClick={onMenuClick}>
+//         내 위키
+//       </Link>
+//       <div className={styles.menuItem} onClick={onLogout}>
+//         로그아웃
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Menu;
+
 'use client';
 
 import Link from 'next/link';
 import styles from './Menu.module.css';
+import { useStore } from '../../store';
 
 interface Props {
   onMenuClick: () => void;
@@ -9,17 +38,37 @@ interface Props {
 }
 
 function Menu({ onMenuClick, onLogout }: Props) {
+  const { isLogin } = useStore((state) => ({
+    isLogin: state.isLogin,
+  }));
+
   return (
     <div className={styles.container}>
-      <Link href="/myAccount" className={styles.menuItem} onClick={onMenuClick}>
-        계정 설정
-      </Link>
-      <Link href="/mypage" className={styles.menuItem} onClick={onMenuClick}>
-        내 위키
-      </Link>
-      <div className={styles.menuItem} onClick={onLogout}>
-        로그아웃
-      </div>
+      {isLogin ? (
+        <>
+          <Link href="/myAccount" className={styles.menuItem} onClick={onMenuClick}>
+            계정 설정
+          </Link>
+          <Link href="/mypage" className={styles.menuItem} onClick={onMenuClick}>
+            내 위키
+          </Link>
+          <div className={styles.menuItem} onClick={onLogout}>
+            로그아웃
+          </div>
+        </>
+      ) : (
+        <>
+          <Link href="/wikilist" className={styles.menuItem} onClick={onMenuClick}>
+            위키 목록
+          </Link>
+          <Link href="/freeBoard" className={styles.menuItem} onClick={onMenuClick}>
+            자유게시판
+          </Link>
+          <Link href="/login" className={styles.menuItem} onClick={onMenuClick}>
+            로그인
+          </Link>
+        </>
+      )}
     </div>
   );
 }
