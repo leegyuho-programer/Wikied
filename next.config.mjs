8ae8a756b881f26be08b2392eb0c63f419468423
Gyuho Lee <j980303@naver.com>
2024-07-04 19:24:57 +0900

    feat: 이미지 srcset, sizes 설정

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,12 @@ const nextConfig = {
         hostname: '*.kr',
       },
     ],
+    // 기기의 너비 중단점 목록
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+
+    // 이미지 가로 너비(px) 목록,
+    // deviceSizes 배열과 연결되어 이미지 srcset 생성하는데 사용
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     // loader: 'imgix',
     // path: '/',
   },
--- src/app/(unauth)/_components/ArticlePage/ArticlePage.tsx
+++ src/app/(unauth)/_components/ArticlePage/ArticlePage.tsx
@@ -117,16 +117,18 @@ export default function ArticlePage() {
           </div>
         </div>
         <ArticleStrokeIcon />
-        <div className={styles.imageWrapper}>
-          <Image
-            src={article.image}
-            alt="대표 이미지"
-            layout="fill"
-            className={styles.image}
-            loading="eager"
-            priority={true}
-          />
-        </div>
+        {/* <div className={styles.imageWrapper}> */}
+        <Image
+          src={article.image}
+          alt="대표 이미지"
+          // layout="fill"
+          width={500}
+          height={400}
+          className={styles.image}
+          loading="eager"
+          priority={true}
+        />
+        {/* </div> */}
         <div>{article.content}</div>
       </div>
       <Link href="/freeBoard" className={styles.link}>
