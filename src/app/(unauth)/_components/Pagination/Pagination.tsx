e7cd2a6c0f617068485085a61ce4a7792156c2fb
Gyuho Lee <j980303@naver.com>
2024-09-19 19:35:54 +0900

    feat: 주석 추가

--- src/app/(unauth)/_components/Pagination/Pagination.tsx
+++ src/app/(unauth)/_components/Pagination/Pagination.tsx
@@ -3,14 +3,14 @@
 import { useState, useEffect } from 'react';
 import styles from './Pagination.module.css';
 
-interface PaginationProps {
-  totalArticles: number;
-  articlesPerPage: number;
-  currentPage: number;
-  onPageChange: (pageNumber: number) => void;
+interface Props {
+  totalArticles: number; // 전체 게시물(기사 등)의 개수
+  articlesPerPage: number; // 한 페이지에 표시할 게시물 수
+  currentPage: number; // 현재 선택된 페이지
+  onPageChange: (pageNumber: number) => void; // 페이지 변경 시 호출될 함수
 }
 
-const Pagination = ({ currentPage, totalArticles, articlesPerPage, onPageChange }: PaginationProps) => {
+const Pagination = ({ currentPage, totalArticles, articlesPerPage, onPageChange }: Props) => {
   const totalPages = Math.ceil(totalArticles / articlesPerPage);
   const [pageGroup, setPageGroup] = useState(Math.ceil(currentPage / 5));
 
@@ -19,9 +19,10 @@ const Pagination = ({ currentPage, totalArticles, articlesPerPage, onPageChange 
   }, [currentPage]);
 
   const getPageNumbers = () => {
-    const start = (pageGroup - 1) * 5 + 1;
-    const end = Math.min(start + 4, totalPages);
-    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    const start = (pageGroup - 1) * 5 + 1; // 페이지 그룹의 첫 번째 번호
+    const end = Math.min(start + 4, totalPages); // 그룹에서 마지막 페이지 번호
+
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i); // 배열로 페이지 번호 리스트 반환
   };
 
   const handlePageChange = (pageNumber: number) => {
@@ -32,14 +33,14 @@ const Pagination = ({ currentPage, totalArticles, articlesPerPage, onPageChange 
 
   const handlePrevGroup = () => {
     if (pageGroup > 1) {
-      const newPage = (pageGroup - 2) * 5 + 1;
+      const newPage = (pageGroup - 2) * 5 + 1; // 이전 그룹의 첫 번째 페이지로 이동
       onPageChange(newPage);
     }
   };
 
   const handleNextGroup = () => {
     if (pageGroup * 5 < totalPages) {
-      const newPage = pageGroup * 5 + 1;
+      const newPage = pageGroup * 5 + 1; // 다음 그룹의 첫 번째 페이지로 이동
       onPageChange(newPage);
     }
   };
