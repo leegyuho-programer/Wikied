a011e054b58d824c5fa1e8d1df78fbc5b46c1913
Gyuho Lee <j980303@naver.com>
2024-09-30 18:40:08 +0900

    fix: 새로고침을 하면 로그아웃이 되는 문제 수정

--- src/app/(auth)/_components/CheckLogin.tsx
+++ src/app/(auth)/_components/CheckLogin.tsx
@@ -6,21 +6,24 @@ import { useStore } from '@/store';
 
 export default function CheckLogin() {
   const router = useRouter();
-  const isLogin = useStore((state) => state.isLogin);
+  const { setLogout } = useStore((state) => ({
+    setLogout: state.setLogout,
+  }));
 
   const handleRedirect = () => {
     if (typeof window === 'undefined') return;
 
     const userAuth = window.localStorage.getItem('store');
 
-    if (!userAuth || !isLogin) {
+    if (!userAuth) {
+      setLogout();
       router.replace('/login');
     }
   };
 
   useEffect(() => {
     handleRedirect();
-  }, [isLogin]);
+  }, []);
 
   return null;
 }
