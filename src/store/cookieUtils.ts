1fc29358c1f6cb46b45a0d4888bbf327f596d8c7
Gyuho Lee <j980303@naver.com>
2024-10-05 19:50:57 +0900

    feat: 쿠키 httpOnly 속성 추가

--- src/store/cookieUtils.ts
+++ src/store/cookieUtils.ts
@@ -4,6 +4,7 @@ export const saveTokenToCookies = (tokenKey: string, tokenValue: string) => {
   setCookie(null, tokenKey, tokenValue, {
     maxAge: 30 * 24 * 60 * 60, // 30일 유지
     path: '/', // 쿠키가 적용되는 URL 경로 (이 경우, 모든 페이지에서 쿠키 접근 가능)
+    httpOnly: true,
     secure: true, // HTTPS 전용
     sameSite: 'strict',
   });
