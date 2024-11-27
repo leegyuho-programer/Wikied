6efbb1043cb5733a4c38b8e0f06959cc401de1ac
Gyuho Lee <j980303@naver.com>
2024-11-28 02:51:13 +0900

    fix: ArticlePage 스캘레톤 수정

--- src/app/(auth)/_components/ArticlePage/ArticlePageSkeleton.tsx
+++ src/app/(auth)/_components/ArticlePage/ArticlePageSkeleton.tsx
@@ -3,10 +3,11 @@ import styles from './ArticlePageSkeleton.module.css';
 export default function ArticlePageSkeleton() {
   return (
     <div className={styles.skeletonContainer}>
-      <div className={`${styles.skeleton} ${styles.skeletonTitle}`}></div>
-      <div className={styles.skeletonSection}>
-        <div className={`${styles.skeleton} ${styles.skeletonSidebar}`}></div>
+      <div className={styles.skeletonContentContainer}>
         <div className={`${styles.skeleton} ${styles.skeletonContent}`}></div>
+      </div>
+      <div className={styles.link}>
+        <div className={`${styles.skeleton} ${styles.skeletonLink}`}></div>
       </div>
     </div>
   );
