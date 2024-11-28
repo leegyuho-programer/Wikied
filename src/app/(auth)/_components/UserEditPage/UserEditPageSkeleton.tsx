40df01519cd9984b903fdc02cc33961656c69192
Gyuho Lee <j980303@naver.com>
2024-11-28 21:40:35 +0900

    refactor: 변수명 변경

--- src/app/(auth)/_components/UserEditPage/UserEditPageSkeleton.tsx
+++ src/app/(auth)/_components/UserEditPage/UserEditPageSkeleton.tsx
@@ -2,8 +2,8 @@ import styles from './UserEditPageSkeleton.module.css';
 
 export default function UserEditPageSkeleton() {
   return (
-    <div className={styles.container}>
-      <div className={`${styles.skeleton} ${styles.skeletontextEditor}`}></div>
+    <div className={styles.skeletonContainer}>
+      <div className={`${styles.skeleton} ${styles.skeletonTextEditor}`}></div>
       {/* <div className={styles.skeletonSideBarContainer}>
         <div className={`${styles.skeleton} ${styles.skeletonSideBar}`}></div>
       </div> */}
