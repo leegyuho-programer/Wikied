6883b7de9cd6eabd854a37ae928f38d9fef88d4b
Gyuho Lee <j980303@naver.com>
2025-02-11 17:09:28 +0900

    fix: 필요없는 svg 제거

--- src/components/SvgComponents/index.ts
+++ src/components/SvgComponents/index.ts
@@ -1,9 +1,6 @@
-export { default as DropDownIcon } from './icons/DropDownIcon/DropDownIcon';
-export { default as WikiIcon } from './icons/WikiIcon/WikiIcon';
 export { default as CloseIcon } from './icons/CloseIcon/CloseIcon';
 export { default as DeleteIcon } from './icons/DeleteIcon/DeleteIcon';
+export { default as DropDownIcon } from './icons/DropDownIcon/DropDownIcon';
 export { default as EditIcon } from './icons/EditIcon/EditIcon';
 export { default as HeartIcon } from './icons/HeartIcon/HeartIcon';
-export { default as NavBarProfileIcon } from './icons/NavBarProfileIcon/NavBarProfileIcon';
-export { default as ProfileIcon } from './icons/ProfileIcon/ProfileIcon';
 export { default as StrokeIcon } from './icons/StrokeIcon/StrokeIcon';
