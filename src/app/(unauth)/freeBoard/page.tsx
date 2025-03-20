946862c95364d07f39bb5faeb8bd114531d9a097
Gyuho Lee <j980303@naver.com>
2025-03-20 19:34:54 +0900

    feat: freeBoardPage 메타데이터 추가

--- src/app/(unauth)/freeBoard/page.tsx
+++ src/app/(unauth)/freeBoard/page.tsx
@@ -1,4 +1,9 @@
+import { metadataMap } from '@/app/metadata';
 import FreeBoardPage from '../_components/FreeBoardPage/FreeBoardPage';
+
+export async function generateMetadata() {
+  return metadataMap.freeBoardPage;
+}
 
 export default function FreeBoard() {
   return <FreeBoardPage />;
