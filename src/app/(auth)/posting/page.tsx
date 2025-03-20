cb189a882541ecc89019adaba01b618e6716ba84
Gyuho Lee <j980303@naver.com>
2025-03-20 19:31:53 +0900

    feat: postingPage 메타데이터 추가

--- src/app/(auth)/posting/page.tsx
+++ src/app/(auth)/posting/page.tsx
@@ -1,4 +1,9 @@
+import { metadataMap } from '@/app/metadata';
 import PostingPage from '../_components/PostingPage/PostingPage';
+
+export async function generateMetadata() {
+  return metadataMap.postingPage;
+}
 
 export default function posting() {
   return <PostingPage />;
